0f8826cb8b6344267b252fa3c3b4d22071737bfa
Luís Felipe <felipe.drs01@gmail.com>
2022-05-21 03:21:43 -0300

    projeto criado

---
+++ src/main/java/com/luisfelipe/projetomongo/ProjetoSpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.luisfelipe.projetomongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringbootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisfelipe/projetomongo/ProjetoSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisfelipe.projetomongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
