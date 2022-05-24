453bafb0150725d69a74044ecf0bc8b72ef77d26
Luís Felipe <felipe.drs01@gmail.com>
2022-05-24 02:42:39 -0300

    obtendo um usuário por ID

--- src/main/java/com/luisfelipe/projetomongo/services/UserService.java
+++ src/main/java/com/luisfelipe/projetomongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.luisfelipe.projetomongo.services;
 
 import com.luisfelipe.projetomongo.domain.User;
 import com.luisfelipe.projetomongo.repository.UserRepository;
+import com.luisfelipe.projetomongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(("Objeto não encontrado!")));
     }
 }
