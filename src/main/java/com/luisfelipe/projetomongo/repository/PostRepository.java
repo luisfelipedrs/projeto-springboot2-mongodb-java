6db81856aa1820569c7f5da0ca81c4718dbe0d2f
Luís Felipe <felipe.drs01@gmail.com>
2022-05-25 05:41:19 -0300

    consulta simples com @Query

--- src/main/java/com/luisfelipe/projetomongo/repository/PostRepository.java
+++ src/main/java/com/luisfelipe/projetomongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.luisfelipe.projetomongo.repository;
 import com.luisfelipe.projetomongo.domain.Post;
 import com.luisfelipe.projetomongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoringCase(String text);
 }
